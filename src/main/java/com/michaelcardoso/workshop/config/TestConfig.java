bf02d4f5c23ec94a9d237dc5a4948a862f1b84e6
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-02-12 09:47:09 -0300

    Payment, one-to-one association

--- src/main/java/com/michaelcardoso/workshop/config/TestConfig.java
+++ src/main/java/com/michaelcardoso/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.michaelcardoso.workshop.entities.Category;
 import com.michaelcardoso.workshop.entities.Order;
 import com.michaelcardoso.workshop.entities.OrderItem;
+import com.michaelcardoso.workshop.entities.Payment;
 import com.michaelcardoso.workshop.entities.Product;
 import com.michaelcardoso.workshop.entities.User;
 import com.michaelcardoso.workshop.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
