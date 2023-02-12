5e3f7797259a89c7bcb11cc3c89e96e522c21a0b
Michael Cardoso <michael.cardoso1810@gmail.com>
2023-02-12 13:35:43 -0300

    User insert

--- src/main/java/com/michaelcardoso/workshop/resources/UserResource.java
+++ src/main/java/com/michaelcardoso/workshop/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.michaelcardoso.workshop.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.michaelcardoso.workshop.entities.User;
 import com.michaelcardoso.workshop.services.UserService;
@@ -28,6 +32,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		User user = userService.insert(obj);
+		URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(location).body(user);
 	}
 	
 }
